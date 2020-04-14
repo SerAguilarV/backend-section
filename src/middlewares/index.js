098c5f049b8678945a846381fcff8c1f343e2bd1
unknown <ser.aguilarv@gmail.com>
2020-04-14 13:58:32 -0500

    [ADD] caching implemented

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,5 +2,6 @@ module.exports = {
     NotFoundMiddleware: require("./notFound.middleware"),
     ErrorMiddleware: require("./error.middleware"),
     AuthMiddleware: require("./auth.middleware"),
-    ParseIntMiddleware: require("./parse-in.middleware")
+    ParseIntMiddleware: require("./parse-in.middleware"),
+    CacheMiddleware: require("./cache.middleware")
 }
